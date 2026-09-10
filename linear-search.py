444982dee17bc86afef15bc56212f231cd50ac26
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-09-10 10:18:30 +0530

    linear-search

---
+++ linear-search.py
@@ -0,0 +1,10 @@
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+
+    return -1
+
+
+arr = [5, 2, 8, 1, 9]
+print(linear_search(arr, 8))
